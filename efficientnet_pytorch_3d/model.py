226272519df053c4ddcad578ac0e8f2c94d7192c
Vedant Kodolkar <93366911+kodolskii@users.noreply.github.com>
2023-12-12 02:14:36 +0530

    Update model.py

--- efficientnet_pytorch_3d/model.py
+++ efficientnet_pytorch_3d/model.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from efficient3d.efficientnet_pytorch_3d.utils.utils import (
+from efficient3d.efficientnet_pytorch_3d.utils import (
     round_filters,
     round_repeats,
     drop_connect,
